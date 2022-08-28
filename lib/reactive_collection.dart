faf2ec5b82f801e990d29d85c3725b4003ec4a16
Christian Thiele <post@thiele-christian.de>
2022-08-28 14:29:10 +0200

    0.1.0-dev.4

--- lib/reactive_collection.dart
+++ lib/reactive_collection.dart
@@ -9,6 +9,7 @@ export 'src/reactive_collection/collection_state_model.dart';
 export 'src/reactive_collection/paged_collection_controller.dart';
 export 'src/reactive_collection/paged_collection_model.dart';
 
+export 'src/reactive_collection/param_wrapper.dart';
 export 'src/reactive_collection/parameterized_collection.dart';
 
 export 'src/reactive_collection/sequential_collection_controller.dart';
